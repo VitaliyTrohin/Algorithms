b889ba874bd8ff0538a7873c615ed5ef95a8f5f7
vetal <vetal195@bk.ru>
2022-08-27 08:50:09 +0300

    DZ_4

---
+++ Trohin_Vitaliy_dz_4/task_4_1.py
@@ -0,0 +1,34 @@
+"""
+Задание 1.
+Приведен код, который позволяет сохранить в
+массиве индексы четных элементов другого массива
+Сделайте замеры времени выполнения кода с помощью модуля timeit
+Попробуйте оптимизировать код, чтобы снизить время выполнения
+Проведите повторные замеры
+ОБЯЗАТЕЛЬНО! Добавьте аналитику: что вы сделали и какой это принесло эффект
+"""
+
+from timeit import timeit
+
+
+def func_1(nums):
+    new_arr = []
+    for i in range(len(nums)):
+        if nums[i] % 2 == 0:
+            new_arr.append(i)
+    return new_arr
+
+
+def func_2(nums):
+    new_arr = [i for i in range(len(nums)) if nums[i] % 2 == 0]
+    return new_arr
+
+
+lst = list(range(100000))
+func_1(lst)
+func_2(lst)
+print(f'Время работы функции {func_1.__name__} - {timeit("func_1(lst)", globals=globals(), number=1000)}')
+print(f'Время работы функции {func_2.__name__} - {timeit("func_2(lst)", globals=globals(), number=1000)}')
+
+# Вторую функцию реализовал с помощью list comprehension, который быстрее цикла for.
+
